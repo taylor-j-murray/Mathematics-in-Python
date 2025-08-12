f11d13645c88326168a86fbe158840eda2f61b74
Taylor Murray <141784046+taylor-j-murray@users.noreply.github.com>
2025-08-12 15:11:18 -0500

    Delete scratch.py

--- scratch.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-A = np.array([[[1,2]],[[3,4]]])
-B = np.array([[[0,0]],[[7,7]]])
-C = np.concatenate([A,B], axis = 0)
-D = np.concatenate([A,B], axis = 2)
-A.append(B)
-print(C)
-print(D)
